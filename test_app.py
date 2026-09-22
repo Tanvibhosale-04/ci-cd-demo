258f6f419f094c9857daa53bef30d125ad528b54
tanvi-bhosale-04 <suvarnabhosale65@gmail.com>
2026-09-22 14:55:44 +0530

    Demo

--- test_app.py
+++ test_app.py
@@ -5,8 +5,6 @@ def test_home():
     client = app.test_client()
 
     response = client.get("/")
-
-    assert response.status_code == 200
     assert response.data == b"Hello from CI/CD Demo!"
 
 
